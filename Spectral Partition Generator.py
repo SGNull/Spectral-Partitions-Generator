fe6431a125fd08b9a741a6f3400aef3832c888ce
SGNull88 <60150561+SGNull88@users.noreply.github.com>
2021-07-27 12:53:13 -0400

    first commit

---
+++ Spectral Partition Generator.py
@@ -0,0 +1,17 @@
+# Imports
+
+# Options
+maximum_size = 100
+
+# Code
+sequence_list = []
+
+total = 0
+stop = False
+nums = 2
+
+tri_minus_three = lambda x : (x*(x+1))/2
+
+while tri_minus_three(nums) <= maximum_size:
+    while total < maximum_size:
+        pass
