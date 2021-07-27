97d49ea06b6a4140545efb3e5dd6d5151fb5e112
SGNull88 <60150561+SGNull88@users.noreply.github.com>
2021-07-27 14:03:43 -0400

    Added/changed a lot. Not working yet, but close.

--- Spectral Partition Generator.py
+++ Spectral Partition Generator.py
@@ -3,15 +3,30 @@
 # Options
 maximum_size = 100
 
-# Code
-sequence_list = []
+# Functions
+def build_sequence(start, length):
+	new_sequence = []
+	for i in range (length):
+		new_sequence.append(start + i)
 
-total = 0
-stop = False
+	return new_sequence
+
+def total_sequence(sequence):
+    total = 0
+    for element in sequence:
+        total += element
+
+    return total
+
+tn = lambda n : int((n*(n+1))/2)
+tri_part_sum = lambda start, length: tn(length + start) - tn(start - 1)
+        
+# Code
+list_of_sequences = []
 nums = 2
 
-tri_minus_three = lambda x : (x*(x+1))/2
-
-while tri_minus_three(nums) <= maximum_size:
-    while total < maximum_size:
-        pass
+while tri_part_sum(3,nums) <= maximum_size:
+    start = 3
+    while tri_part_sum(start, nums) <= maximum_size:
+        
+        list_of_sequences.append(tuple(build_sequence(start, nums), tri_part_sum(start, nums)))
