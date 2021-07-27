34a2d7e1d7b914ec861ad63c14a45cddc6f146f6
SGNull88 <60150561+SGNull88@users.noreply.github.com>
2021-07-27 17:15:03 -0400

    Added user input, removed unecessary stuff.

--- Spectral Partition Generator.py
+++ Spectral Partition Generator.py
@@ -1,41 +1,16 @@
 # Imports
-import csv
+from csv import writer
 
-# Options
-DEPTH = 120   # Total numerical increments
-WIDTH = 60   # Maximum amount of numbers in the sequences
-OUTPUT_NAME = "output_test"
+# Functions
+tn = lambda n : int((n*(n+1))/2)
+tri_part_sum = lambda start, length: tn(length + start - 1) - tn(start - 1)
 
-# Code
 def build_sequence_list(start, length):
 	new_sequence = []
 	for i in range (length):
 		new_sequence.append(start + i)
 
 	return new_sequence
-
-tn = lambda n : int((n*(n+1))/2)
-tri_part_sum = lambda start, length: tn(length + start - 1) - tn(start - 1)
-
-def build_original_list(maximum_size):
-    list_of_sequences = []   # Example structure of this list after the code is ran: [([1,2,3], 6), ([2,3,4], 9), ([3,4,5], 12), ...]
-
-    nums = 2
-    start = 3
-    total = tri_part_sum(start, nums)
-
-    # with the following code simplified, it does look rather strange. These are seperate while loops, because each one modifies the total in a different way.
-    while total <= maximum_size:
-        while total <= maximum_size:
-            list_of_sequences.append((build_sequence_list(start, nums), total))
-            start += 1
-            total += nums
-
-        nums += 1
-        start = 3
-        total = tri_part_sum(start, nums)
-    
-    return list_of_sequences
         
 def build_matrix_list(max_increments, max_length):
     list_of_lists_of_sequences = [] # Example structure: [[[1,2,3], 6, [1,2,3,4], 10, [1,2,3,4,5], 15, ...], [[2,3,4], 9, [2,3,4,5], 14, [2,3,4,5,6], 20], ...]
@@ -53,11 +28,18 @@ def build_matrix_list(max_increments, max_length):
     
     return list_of_lists_of_sequences
 
-def matrix_output_csv(big_ol_matrix):
-    with open(OUTPUT_NAME + ".csv", "w", newline='') as output_file:
-        output = csv.writer(output_file, )
+def matrix_output_csv(big_ol_matrix, output_name):
+    with open(output_name + ".csv", "w", newline='') as output_file:
+        output = writer(output_file, )
         output.writerows(big_ol_matrix)
 
-output_matrix = build_matrix_list(DEPTH, WIDTH)
+# Code
+depth = int(input("Enter the number of times sequences will be incremented: "))
+width = int(input("Enter the maximum length of the sequences: "))
+output_name = input("Enter the name of the output: ")
 
-matrix_output_csv(output_matrix)
+print("Generating data...")
+output_matrix = build_matrix_list(depth, width)
+
+print("Writing data to " + output_name + ".csv...")
+matrix_output_csv(output_matrix, output_name)
